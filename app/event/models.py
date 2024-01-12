b43f93d0c27521d7dbd6d8681fbee8e8422255d4
kolaxy <vybornoff@outlook.com>
2024-01-12 20:06:04 +0300

    added admin creation for admin

--- app/event/models.py
+++ app/event/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Organization(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    address = models.CharField(max_length=255)
+    postcode = models.CharField(max_length=10)
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    organizations = models.ManyToManyField(Organization)
+    image = models.ImageField(upload_to='vol/')
+    date = models.DateTimeField()
