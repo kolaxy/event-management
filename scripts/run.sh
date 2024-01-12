542b8eadf0ed4a9647d0695e88a409f470ff219f
kolaxy <vybornoff@outlook.com>
2024-01-12 20:10:27 +0300

    sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -14,4 +14,15 @@ done
 
 python manage.py makemigrations
 python manage.py migrate
+
+
+if python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='admin', is_superuser=True).exists())" | grep "True"; then
+    echo "superuser is OK"
+else
+    echo "No superuser"
+    python manage.py createsuperuser --email=admin@admin.com --noinput
+    echo "Superuser admin/admin was created"
+fi
+
+
 python manage.py runserver 0.0.0.0:8000
