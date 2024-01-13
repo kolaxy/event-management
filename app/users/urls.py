a7a317aac029e6506daa3388c6146e6df30e5829
kolaxy <vybornoff@outlook.com>
2024-01-13 14:19:58 +0300

    jwt bearer test endpoint

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,7 +1,8 @@
 # urls.py
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, me
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
+    path('me/', me, name='me')
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 class UserRegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -14,3 +17,15 @@ class UserRegistrationView(generics.CreateAPIView):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def me(request):
+    """
+    Function to test JWT
+    Returns user's email 
+    """
+    email = None
+    if request.user.is_authenticated:
+        email = request.user.email
+    return Response({"email": email})
