4722c831295169ab49532a31fa31162992182e29
kolaxy <vybornoff@outlook.com>
2024-01-13 15:18:11 +0300

    search by date period

--- app/event/views.py
+++ app/event/views.py
@@ -1,9 +1,11 @@
+import datetime
 from django.shortcuts import render
+from django.utils import timezone
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.filters import OrderingFilter, SearchFilter
-
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .models import Organization, Event
 from .serializers import OrganizationSerializer, EventSerializer
 
@@ -29,3 +31,19 @@ class EventListView(generics.ListAPIView):
     ordering_fields = ['date']
     search_fields = ['title']
     pagination_class = CustomEventPagination
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        start_date = self.request.query_params.get('start_date', None)
+        end_date = self.request.query_params.get('end_date', None)
+
+        if start_date and end_date:
+            try:
+                start_date = timezone.make_aware(timezone.datetime.strptime(start_date, '%Y-%m-%d'))
+                end_date = timezone.make_aware(timezone.datetime.strptime(end_date, '%Y-%m-%d'))
+                queryset = queryset.filter(date__range=(start_date, end_date))
+            except ValueError:
+                return Response({'error': 'Invalid date format'}, status=400)
+
+        return queryset
