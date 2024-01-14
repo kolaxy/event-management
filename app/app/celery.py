66a9f896f9ab26a10e3e12f8aa2e7c78a6320a37
kolaxy <vybornoff@outlook.com>
2024-01-14 14:36:33 +0300

    celery conf file updated

--- app/app/celery.py
+++ app/app/celery.py
@@ -1,10 +1,33 @@
-import os
+# import os
 
+# from celery import Celery
+
+# # Set the default Django settings module for the 'celery' program.
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+
+# app = Celery('app')
+
+# # Using a string here means the worker doesn't have to serialize
+# # the configuration object to child processes.
+# # - namespace='CELERY' means all celery-related configuration keys
+# #   should have a `CELERY_` prefix.
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# # Load task modules from all registered Django apps.
+# app.autodiscover_tasks()
+
+
+# @app.task(bind=True, ignore_result=True)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
+
+import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
+# Create a Celery application instance
 app = Celery('app')
 
 # Using a string here means the worker doesn't have to serialize
@@ -16,7 +39,17 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+# Reading environment variables for Celery configuration
+BROKER_URL = os.getenv('REDIS_URL')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL')
+CELERY_TIMEZONE = os.getenv('CELERY_TIMEZONE')
 
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+# Update Celery configuration with environment variables
+app.conf.update(
+    BROKER_URL=BROKER_URL,
+    CELERY_RESULT_BACKEND=CELERY_RESULT_BACKEND,
+    CELERY_TIMEZONE=CELERY_TIMEZONE,
+    CELERY_ACCEPT_CONTENT=['application/json'],
+    CELERY_TASK_SERIALIZER='json',
+    CELERY_RESULT_SERIALIZER='json',
+)
